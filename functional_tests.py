389e6c96ee894344a287cbe64271d42fbb473ee5
Ludovic Perseghini <perseghini@gmail.com>
2016-05-13 17:39:33 -0700

    more commit

--- functional_tests.py
+++ functional_tests.py
@@ -12,14 +12,19 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_elements_by_name('h1').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        input_box = self.browser.find_element_by_name('id_new_item')
+        input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             input_box.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -28,11 +33,8 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_elements_by_tag_name('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
 
